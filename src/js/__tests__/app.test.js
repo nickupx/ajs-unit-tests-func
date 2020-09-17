1076dc848ad2352212871e7603f6af3344a7b983
nickupx <nick.upsky@gmail.com>
2020-09-18 01:05:35 +0300

    sort function and tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,5 @@
 import sum from '../basic';
-import showHealth from '../app';
+import showHealth, { sortByHealth } from '../app';
 
 test('should sum', () => {
   const result = sum([1, 2, 3]);
@@ -39,4 +39,34 @@ test('should be critical', () => {
 test('should be critical', () => {
   const result = showHealth({ name: 'Маг', health: 0 });
   expect(result).toBe('critical');
+});
+
+test('should be sorted', () => {
+  const result = sortByHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
+
+test('should be sorted', () => {
+  const result = sortByHealth([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'рыцарь', health: 0 },
+    { name: 'гном', health: 100500 },
+  ]);
+  expect(result).toEqual([
+    { name: 'гном', health: 100500 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'рыцарь', health: 0 },
+  ]);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,8 @@ export default function showHealth(obj) {
   if (obj.health <= 50 && obj.health >= 15) return 'wounded';
   return 'critical';
 }
+
+export function sortByHealth(arr) {
+  arr.sort((a, b) => (a.health > b.health ? -1 : 1));
+  return arr;
+}
